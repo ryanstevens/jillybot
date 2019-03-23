5ed63a41b70c028eeea87cbe076e676d2dca705a
Ryan Stevens <stevens.ryan@gmail.com>
2019-03-23 16:55:56 -0700

    2

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ var servolib = require('servo-pca9685');
 var servo = servolib.use(tessel.port['A']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
+var servo2 = 5;
+
 
 servo.on('ready', function () {
   var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
@@ -28,6 +30,21 @@ servo.on('ready', function () {
       console.log('Position (in range 0-1):', position);
       //  Set servo #1 to position pos.
       servo.move(servo1, position);
+
+      // Increment by 10% (~18 deg for a normal servo)
+      position += 0.1;
+      if (position > 1) {
+        position = 0; // Reset servo position
+      }
+    }, 500); // Every 500 milliseconds
+  });
+
+  servo.configure(servo2, 0.05, 0.12, function () {
+    setInterval(function () {
+      console.log('Position (in range 0-1):', position);
+      //  Set servo #1 to position pos.
+
+      servo.move(servo2, position);
 
       // Increment by 10% (~18 deg for a normal servo)
       position += 0.1;
